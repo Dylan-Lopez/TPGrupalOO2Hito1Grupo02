d7773e9c2458034753259d92d3a06eea6624cca6
Martin <martin.lucarelli94@gmail.com>
2026-08-31 16:34:38 -0300

    refactor files

--- src/test/PedidoTests/TestActualizarPedido.java
+++ src/test/PedidoTests/TestActualizarPedido.java
@@ -1,4 +1,4 @@
-package test;
+package test.PedidoTests;
 
 import java.time.LocalDate;
 
--- src/test/PedidoTests/TestEliminarPedido.java
+++ src/test/PedidoTests/TestEliminarPedido.java
@@ -1,4 +1,4 @@
-package test;
+package test.PedidoTests;
 
 import datos.Pedido;
 import negocio.PedidoABM;
